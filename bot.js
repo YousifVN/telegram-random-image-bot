1ef84cd05c874f2319907f1700e07c946e19f4c7
YousifVN <yousifmahmood995@gmail.com>
2025-03-08 21:35:36 +0300

    [update] stopped asking for subscription and added new images

--- bot.js
+++ bot.js
@@ -4,21 +4,11 @@ const fs = require('fs');
 const path = require('path');
 
 const token = process.env.BOT_TOKEN;
-const CHANNEL_USERNAME = '@YousifCoding';
 const DEVELOPER_BOT = '@VNN3Bot';
 const BOT_USERNAME = '@VNTummyBot';
+const CHANNEL_USERNAME = '@YousifCoding';
 
 const bot = new TelegramBot(token, { polling: true });
-
-async function isSubscribed(userId) {
-    try {
-        const chatMember = await bot.getChatMember(CHANNEL_USERNAME, userId);
-        return ['member', 'administrator', 'creator'].includes(chatMember.status);
-    } catch (error) {
-        console.error('Error checking subscription:', error);
-        return false;
-    }
-}
 
 function getRandomImage() {
     const imagesDir = path.join(__dirname, 'images');
@@ -33,15 +23,6 @@ bot.onText(/\/random/, async (msg) => {
     const userId = msg.from.id;
 
     try {
-        const subscribed = await isSubscribed(userId);
-        
-        if (!subscribed) {
-            return bot.sendMessage(
-                chatId,
-                `لازم تشترك بالقناة ${CHANNEL_USERNAME} قبل ما تگدر تستخدم البوت!`
-            );
-        }
-
         const imagePath = getRandomImage();
         await bot.sendPhoto(chatId, imagePath);
         await bot.sendMessage(chatId, 'مبروك تعبئة كرشك 😁، دز /random اذا تريد بعد وحدة!');
@@ -58,8 +39,7 @@ bot.onText(/\/start/, (msg) => {
         chatId,
         "هلو هلو 👋\n\n" +
         "حتى تگدر تستخدم البوت لازم:\n" +
-        `1. تشترك بالقناة ${CHANNEL_USERNAME}\n` +
-        "2. تدز /random حتى تحصل صور عشوائية\n\n" +
+        "تدز /random حتى تحصل صور عشوائية\n\n" +
         "مبروك تعبئة كرشك 😁"
     );
 });
@@ -81,7 +61,7 @@ bot.onText(/\/help/, (msg) => {
         chatId,
         "الاوامر المتوفرة: 📌\n\n" +
         "🔹 /start - رسالة الترحيب والتعليمات الاساسية\n" +
-        "🔹 /random - يطلعلك صورة عشوائية (لازم تشترك بالقناة)\n" +
+        "🔹 /random - يطلعلك صورة عشوائية\n" +
         "🔹 /contact - معلومات التواصل وياي\n" +
         "🔹 /help - يطلعلك هاي الرسالة\n\n" +
         "📱 شلون تستخدم البوت بالخاص:\n" +
@@ -90,9 +70,7 @@ bot.onText(/\/help/, (msg) => {
         "1. حط '@' و يوزر البوت بعد الامر (مثال: /random" + BOT_USERNAME + ")\n" +
         "2. البوت يرد بس اذا:\n" +
         "   • تسوي منشن " + BOT_USERNAME + "\n" +
-        "   • ترد على رسالة من رسائل البوت\n\n" +
-        "⭐️ الشروط:\n" +
-        "لازم تشترك بقناة " + CHANNEL_USERNAME + " حتى تگدر تستخدم امر /random"
+        "   • ترد على رسالة من رسائل البوت\n\n"
     );
 });
 
